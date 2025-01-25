3285ded550340234421255107cac1eaf0d51c872
Mahdi Abedini <m.abedini30@yahoo.com>
2025-01-25 12:08:08 +0330

    simple c implementation

---
+++ matrix.c
@@ -0,0 +1,16 @@
+#define IND(i, j) ((i)*n + (j))
+
+void matrix_mul(int* m1, int* m2, int* dest, int n) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            dest[IND(i, j)] = 0;
+            for (int k = 0; k < n; k++)
+            {
+                dest[IND(i, j)] += m1[IND(i, k)] * m2[IND(k, j)];
+            }
+        }
+    }
+    return;
+}
