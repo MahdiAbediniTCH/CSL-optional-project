a6f5ddabdd944f42e371a8a3d28f82ed060cb630
Mahdi Abedini <m.abedini30@yahoo.com>
2025-01-26 14:09:55 +0330

    Update plot.py to read from  binary

--- plot.py
+++ plot.py
@@ -1,9 +1,24 @@
 import matplotlib.pyplot as plt
 
-input_sizes = list(range(4, 2049, 4))
-runtimes_program1 = list(range(1,513))
-runtimes_program2 = list(range(4,2049, 4))
-runtimes_program3 = list(range(1,513))
+import struct
+
+MAX_N = 512
+N_RESULTS = MAX_N // 4
+def read_results(file_name):
+
+    with open(file_name, "rb") as file:
+        data = file.read()
+
+    num_doubles = N_RESULTS
+    format_string = f"{num_doubles}d"
+
+    return list(struct.unpack(format_string, data))
+
+
+input_sizes = list(range(4, MAX_N + 1, 4))
+runtimes_program1 = read_results("results_gcc_O0.bin")
+runtimes_program2 = read_results("results_gcc_O3.bin")
+runtimes_program3 = read_results("results_matrix_mul_function.s.bin")
 
 # Check that the lists are all the same length
 assert len(input_sizes) == len(runtimes_program1) == len(runtimes_program2) == len(runtimes_program3)
@@ -18,9 +33,9 @@ fig, ax = plt.subplots()
 colors = ['#4C72B0', '#55A868', '#C44E52']
 
 # Plot the runtimes with distinct styles (without markers)
-ax.plot(input_sizes, runtimes_program1, linestyle='-', color=colors[0], label='Program 1')
-ax.plot(input_sizes, runtimes_program2, linestyle='--', color=colors[1], label='Program 2')
-ax.plot(input_sizes, runtimes_program3, linestyle='-.', color=colors[2], label='Program 3')
+ax.plot(input_sizes, runtimes_program1, color=colors[0], label='GCC -O0')
+ax.plot(input_sizes, runtimes_program2, color=colors[1], label='GCC -O3')
+ax.plot(input_sizes, runtimes_program3, color=colors[2], label='Linear assembly')
 
 # Add shadow effect
 ax.fill_between(input_sizes, runtimes_program1, alpha=0.1, color=colors[0])
